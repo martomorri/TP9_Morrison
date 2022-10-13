22c4a4876801794f17a32d259adfc2cca82f8e90
martomorri <106319796+martomorri@users.noreply.github.com>
2022-10-13 10:23:17 -0300

    Clases

---
+++ Models/Compra.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP9_Morrison.Models;
+public class Compra
+{
+    private int _id, _id_usuario, _id_producto;
+    private string _nombre, _apellido, _num_tarjeta, _cod_seguridad;
+    private DateTime _fecha_vencimiento;
+    public Compra(int id, int id_usuario, int id_producto, string nombre, string apellido, string num_tarjeta, DateTime fecha_vencimiento, string cod_seguridad)
+    {
+        _id = id; _id_usuario = id_usuario; _id_producto = id_producto; _nombre = nombre; _apellido = apellido; _num_tarjeta = num_tarjeta; _fecha_vencimiento = fecha_vencimiento; _cod_seguridad = cod_seguridad;
+    }
+    public Compra()
+    {
+        _id = 0; _id_usuario = 0; _id_producto = 0; _nombre = ""; _apellido = ""; _num_tarjeta = ""; _fecha_vencimiento = new DateTime(1,1,1); _cod_seguridad = "";
+    }
+    public int id
+    {
+        get { return _id; }
+    }
+    public int id_usuario
+    {
+        get { return _id_usuario; }
+    }
+    public int id_producto
+    {
+        get { return _id_producto; }
+    }
+    public string nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+    public string apellido
+    {
+        get { return _apellido; }
+        set { _apellido = value; }
+    }
+    public string num_tarjeta
+    {
+        get { return _num_tarjeta; }
+        set { _num_tarjeta = value; }
+    }
+    public DateTime fecha_vencimiento
+    {
+        get { return _fecha_vencimiento; }
+        set { _fecha_vencimiento = value; }
+    }
+    public string cod_seguridad
+    {
+        get { return _cod_seguridad; }
+        set { _cod_seguridad = value; }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP9_Morrison.Models;
+public class Producto
+{
+    private int _id, _id_categoria, _stock_actual, _id_marca;
+    private string _nombre, _descripcion, _articulo, _imagen, _color, _talle, _categoria, _marca;
+    private double _precio;
+    public Producto(int id, string nombre, string descripcion, string articulo, int id_categoria, string imagen, double precio, string color, string talle, int stock_actual, int id_marca, string categoria, string marca)
+    {
+        _id = id; _nombre = nombre; _descripcion = descripcion; _articulo = articulo; _id_categoria = id_categoria; _imagen = imagen; _precio = precio; _color = color; _talle = talle; _stock_actual = stock_actual; _id_marca = id_marca; _categoria = categoria; _marca = marca;
+    }
+    public Producto() 
+    {
+        _id = 0; _nombre = ""; _descripcion = ""; _articulo = ""; _id_categoria = 0; _imagen = ""; _precio = 0; _color = ""; _talle = ""; _stock_actual = 0; _id_marca = 0; _categoria = ""; _marca = "";
+    }
+    public int id
+    {
+        get { return _id; }
+    }
+    public string nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+    public string descripcion
+    {
+        get { return _descripcion; }
+        set { _descripcion = value; }
+    }
+    public string articulo
+    {
+        get { return _articulo; }
+        set { _articulo = value; }
+    }
+    public int id_categoria
+    {
+        get { return _id; }
+    }
+    public string imagen
+    {
+        get { return _imagen; }
+        set { _imagen = value; }
+    }
+    public double precio
+    {
+        get { return _precio; }
+        set { _precio = value; }
+    }
+    public string color
+    {
+        get { return _color; }
+        set { _color = value; }
+    }
+    public string talle
+    {
+        get { return _talle; }
+        set { _talle = value; }
+    }
+    public int stock_actual
+    {
+        get { return _id; }
+        set { _stock_actual = value; }
+    }
+    public int id_marca
+    {
+        get { return _id; }
+    }
+    public string categoria
+    {
+        get { return _categoria; }
+        set { _categoria = value; }
+    }
+    public string marca
+    {
+        get { return _marca; }
+        set { _marca = value; }
+    }
+}
